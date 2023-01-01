f14ca634397ed7718bb2cc0e0b81bf25525a8c59
cl8396@gmail.com <cl8396@gmail.com>
2023-01-01 18:21:27 +0000

    added functionality using javascript + some styles

---
+++ index.js
@@ -0,0 +1,40 @@
+const submitButton = document.querySelector('.js-submit-button');
+
+const ratingState = document.querySelector('.rating-state');
+const thanksState = document.querySelector('.thank-you-state');
+
+const ratingScoreText = document.querySelector('.rating-score-span');
+
+const ratingButtons = document.querySelectorAll('input[name="rating-score"]');
+
+
+function submitRating() {
+    if (document.querySelector('input[name="rating-score"]:checked') === null) { /* ensures a score is selected */
+        alert('please select a score, twat');
+        return;
+    } 
+    updateRatingText()
+    changeActiveState()
+}
+
+function updateRatingText() {
+    let selectedRating;
+    for (const ratingButton of ratingButtons) {
+        if (ratingButton.checked) {
+            selectedRating = ratingButton.value;
+            ratingScoreText.textContent = selectedRating;
+            break;
+        }
+    }
+}
+
+function changeActiveState() {
+    if (ratingState.getAttribute('data-active') === 'true') {
+        ratingState.setAttribute('data-active', 'false');
+        thanksState.setAttribute('data-active', 'true');
+    }
+    return;
+}
+
+submitButton.addEventListener('click', submitRating)
+
